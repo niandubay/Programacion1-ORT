ff5e068c97beca245855d6d27028f57a417b0a1b
juanch0 <juanch0@juanch0-w8>
2015-04-27 00:17:12 -0300

    Ejercicio 13 del Practico 3 hecho!

--- public_html/Practico3/js/js13.js
+++ public_html/Practico3/js/js13.js
@@ -1,2 +1,20 @@
 // Ejercicio 13
 //
+function mostrar() {
+    var digito = 0;
+    var numero = 0;
+    numero = parseInt($("#numero").val());
+    if (numero < 0) {
+        numero = numero * -1;
+    }
+    if (numero === 0) {
+        digito = 1;
+    } else {
+        while (numero >= 1) {
+            numero = numero / 10;
+            digito++;
+        }
+    }
+    $("#salida").html("El número tiene " + digito + " digitos.");
+}
+$("#mostrar").click(mostrar);

2843014b3379e5d828eb909dc27377cd9e283327
juanch0 <niandubay79@gmail.com>
2015-05-30 21:22:24 -0300

    Práctico 6 Completo.

--- public_html/Practico6/js/js9.js
+++ public_html/Practico6/js/js9.js
@@ -1,20 +1,22 @@
-// Ejercicio 8
+// Ejercicio 9
 //
 var primerArray = new Array(1,1);
 function fribonacci(_array) {
     var _segundoArray = _array.slice(0);
-    for (var i = _segundoArray.length-1; i < 20; i++) {
+    var i = _segundoArray.length-1;
+    while (_segundoArray[i]<1000){
         var _numeroMayor = _segundoArray[i];
         var _numeroMenor = _segundoArray[i-1];
         var _suma = _numeroMayor + _numeroMenor;
         _segundoArray.push(_suma);
+        i++;
     }
     return _segundoArray;
 }
 function mostrar(){
     $("#salida").html("");
     var _segundoArray = fribonacci(primerArray);    
-    for (var j = 0; j < _segundoArray.length; j++) {
+    for (var j = 0; j < _segundoArray.length-1; j++) {
         $("#salida").append(_segundoArray[j]+"<br>");
     }
 }
